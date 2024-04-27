266b8e9eeb7986bb7f3d3283a8bc06f38b18f8e6
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-04-27 19:19:52 -0300

    creating repositories

---
+++ DependencyStore/Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿
+using DependencyStore.Models;
+using System.Data.SqlClient;
+using Dapper;
+using DependencyStore.Repositories.Contracts;
+
+namespace DependencyStore.Repositories
+{
+    public partial class CustomerRepository : ICustomerRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public CustomerRepository(SqlConnection connection) => _connection = connection;
+
+        public async Task<Customer?> GetByIdAsync(string customerId)
+        {
+            const string query = "SELECT [Id], [Name], [Email] FROM CUSTOMER WHERE ID=@id";
+
+            return await _connection.QueryFirstAsync<Customer>(query, new { id = customerId });
+        }
+    }
+}
