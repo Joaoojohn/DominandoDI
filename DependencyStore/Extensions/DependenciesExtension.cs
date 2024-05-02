9e565f391c0bda1222d89204642b46d4729fe21c
Joaoojohn <joaopedro.cs12@hotmail.com>
2024-05-02 00:07:51 -0300

    dependency injection

---
+++ DependencyStore/Extensions/DependenciesExtension.cs
@@ -0,0 +1,25 @@
+﻿using DependencyStore.Repositories.Contracts;
+using DependencyStore.Repositories;
+using DependencyStore.Services.Constracts;
+using DependencyStore.Services;
+using System.Data.SqlClient;
+
+namespace DependencyStore.Extensions
+{
+    public static class DependenciesExtension
+    {
+        public static void AddSqlConnection(this IServiceCollection services, string connectionString) 
+            => services.AddScoped(con => new SqlConnection(connectionString));
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDeliveryFeesService, DeliveryFeeService>();
+        }
+    }
+}
